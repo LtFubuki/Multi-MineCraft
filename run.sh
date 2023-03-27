fd39e5497d5c4827a2f68e2c8300fb64ca501e8f
LtFubuki <99317076+LtFubuki@users.noreply.github.com>
2023-03-26 20:45:18 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,16 +6,6 @@ DOCKER_IMAGE_NAME=minecraft-server
 # Define the name of the new Docker container
 DOCKER_CONTAINER_NAME=minecraft-server
 
-# Define the URLs of the configuration files
-SERVER_PROPERTIES_URL=https://raw.githubusercontent.com/LtFubuki/Multi-MineCraft/main/server.properties
-GEYSER_CONFIG_URL=hhttps://raw.githubusercontent.com/LtFubuki/Multi-MineCraft/main/geyser-config.yml
-FLOODGATE_CONFIG_URL=https://raw.githubusercontent.com/LtFubuki/Multi-MineCraft/main/floodgate-config.yml
-
-# Download the configuration files
-wget $SERVER_PROPERTIES_URL -O server.properties
-wget $GEYSER_CONFIG_URL -O geyser-config.yml
-wget $FLOODGATE_CONFIG_URL -O floodgate-config.yml
-
 # Build the Docker image
 docker build -t $DOCKER_IMAGE_NAME https://github.com/LtFubuki/Multi-MineCraft.git#main
 
@@ -23,11 +13,20 @@ docker build -t $DOCKER_IMAGE_NAME https://github.com/LtFubuki/Multi-MineCraft.g
 docker stop $DOCKER_CONTAINER_NAME
 docker rm $DOCKER_CONTAINER_NAME
 
+# Download the configuration files and start script
+wget https://raw.githubusercontent.com/LtFubuki/Multi-MineCraft/main/server.properties
+wget https://raw.githubusercontent.com/LtFubuki/Multi-MineCraft/main/geyser-config.yml
+wget https://raw.githubusercontent.com/LtFubuki/Multi-MineCraft/main/floodgate-config.yml
+wget https://raw.githubusercontent.com/LtFubuki/Multi-MineCraft/main/start.sh
+chmod +x start.sh
+
 # Run the new Docker container
 docker run -d \
   --name $DOCKER_CONTAINER_NAME \
-  -p 25565:25565 -p 19132:19132/udp \
-  -v $(pwd)/server.properties:/minecraft/server.properties \
-  -v $(pwd)/geyser-config.yml:/minecraft/geyser-config.yml \
-  -v $(pwd)/floodgate-config.yml:/minecraft/floodgate-config.yml \
+  -p 25565:25565 \
+  -p 19132:19132/udp \
+  -v "$(pwd)/server.properties:/minecraft/server.properties" \
+  -v "$(pwd)/geyser-config.yml:/minecraft/geyser-config.yml" \
+  -v "$(pwd)/floodgate-config.yml:/minecraft/floodgate-config.yml" \
+  -v "$(pwd)/start.sh:/minecraft/start.sh" \
   $DOCKER_IMAGE_NAME
