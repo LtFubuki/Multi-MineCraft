7ba3e0d2ca41d39df5cc7f21e2edd3c350f7a01f
LtFubuki <99317076+LtFubuki@users.noreply.github.com>
2023-04-10 20:37:58 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start GeyserMC and Floodgate
-java -jar mods/Geyser.jar & java -jar mods/Floodgate.jar &
+java -jar Geyser.jar & java -jar Floodgate.jar &
 
 # Start the Minecraft server
 exec java -Xmx2G -jar minecraft_server.jar --nogui
